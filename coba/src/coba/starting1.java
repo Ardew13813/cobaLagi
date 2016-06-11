ebfc98cc91ee83af17a551d2ef076b2b0582b1fd
Ardhit <guax.raa@gmail.com>
2016-06-11 14:02:26 +0700

    update1

---
+++ coba/src/coba/starting1.java
@@ -0,0 +1,15 @@
+package coba;
+
+public class starting1 {
+	public String utama ="kjashdkahsd";
+	public void mencoba(){
+		
+		String opo = "gelisah";
+		System.out.printf(utama + opo);
+		
+	}
+
+	}
+		
+
+
---
+++ coba/src/coba/starting2.java
@@ -0,0 +1,13 @@
+package coba;
+
+public class starting2 {
+	public static void main(String[] hahaha){
+		
+		starting1 ingin = new starting1();
+		ingin.mencoba();
+		
+	}
+		
+		
+	}
+
